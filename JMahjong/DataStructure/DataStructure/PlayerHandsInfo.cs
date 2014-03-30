d1e37c5860b30322ab9140077022a09721410515
Qingyu Sui <sqybi@126.com>
2014-03-30 23:11:55 +0800

    Merge player hands info into one class; add definition of yaku interface

--- JMahjong/DataStructure/DataStructure/PlayerHandsInfo.cs
+++ JMahjong/DataStructure/DataStructure/PlayerHandsInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace JMahjong.General.DataStructure
+﻿using System.Collections.Generic;
+
+namespace JMahjong.General.DataStructure
 {
     /// <summary>
     /// 和玩家相关的手牌信息
@@ -6,13 +8,43 @@
     public class PlayerHandsInfo
     {
         /// <summary>
-        /// 牌面信息
+        /// 浮动牌（未吃/碰/明杠/暗杠过的牌）
         /// </summary>
-        public HandsInfo Hands { get; set; }
+        public List<TileInfo> FreeTileList { get; set; }
 
         /// <summary>
-        /// 玩家场况信息
+        /// 固定牌组（吃/碰/明杠/暗杠过的牌组）
         /// </summary>
-        public PlayerSituationInfo Situation { get; set; }
+        public List<TilesInfo> StickedTilesList { get; set; }
+
+        /// <summary>
+        /// 最后进张牌（同时存在于FreeTileList中）
+        /// </summary>
+        public LatestTileInfo LatestTile { get; set; }
+
+        /// <summary>
+        /// 场风（东/南/西/北）
+        /// </summary>
+        public int PrevailingWind { get; set; }
+
+        /// <summary>
+        /// 自风（东/南/西/北）
+        /// </summary>
+        public int OwnWind { get; set; }
+
+        /// <summary>
+        /// 宝牌（不包含里宝牌）
+        /// </summary>
+        public List<TileInfo> Dora { get; set; }
+
+        /// <summary>
+        /// 里宝牌
+        /// </summary>
+        public List<TileInfo> UraDora { get; set; }
+
+        /// <summary>
+        /// 是否为一发巡
+        /// </summary>
+        public bool IsOneShotRound { get; set; }
     }
 }
---
+++ JMahjong/ValueCalculation/Yaku/IYaku.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using JMahjong.General.DataStructure;
+
+namespace JMahjong.ValueCalculation.Yaku
+{
+    /// <summary>
+    /// 役的接口
+    /// </summary>
+    public interface IYaku
+    {
+        /// <summary>
+        /// 根据玩家手牌情况计算对应番数
+        /// </summary>
+        /// <param name="playerHands">玩家的完整手牌情况</param>
+        /// <returns>该役应得的番数，0为无此役</returns>
+        int GetHanByPlayerHands(PlayerHandsInfo playerHands);
+    }
+}
