dc1b6c0159cf4551a9a1332c001241923cf93185
Qingyu Sui <sqybi@126.com>
2014-03-30 21:46:25 +0800

    Define Tile

---
+++ JMahjong/DataStructure/Tile.cs
@@ -0,0 +1,45 @@
+﻿namespace JMahjong.DataStructure
+{
+    /// <summary>
+    /// 麻将牌的类型
+    /// </summary>
+    public enum TileType
+    {
+        /// <summary>
+        /// 筒子
+        /// </summary>
+        Pin,
+        /// <summary>
+        /// 索子
+        /// </summary>
+        Sou,
+        /// <summary>
+        /// 万子
+        /// </summary>
+        Wan,
+        /// <summary>
+        /// 风牌
+        /// </summary>
+        Wind,
+        /// <summary>
+        /// 三元牌
+        /// </summary>
+        Dragon
+    }
+
+    /// <summary>
+    /// 定义单张麻将牌
+    /// </summary>
+    public class Tile
+    {
+        /// <summary>
+        /// 类型（桶/索/万/风/三元）
+        /// </summary>
+        public TileType Type { get; set; }
+
+        /// <summary>
+        /// 数字（1..4 => 东/南/西/北；1..3 => 白/发/中）
+        /// </summary>
+        public int Number { get; set; }
+    }
+}
